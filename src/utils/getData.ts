5675605b2b41bedc2dbd4d854ed22d1ea83b7511
Aldo Camacho <aldo.acamacho98@gmail.com>
2021-06-03 15:43:55 -0700

    Maybe :)

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,16 +1,14 @@
 const API = 'https://www.amiiboapi.com/api/';
 
 const getData = async (id?: string) => {
-	const apiURL = id ? `${API}${id}` : API;
+	const apiURL = id ? `${API}${id}/` : API;
 	try {
 		console.log(apiURL);
 		const response = await fetch(apiURL);
-		console.log(":)")
-		console.log(response);
 		const data = await response.json();
 		return data;
 	} catch (error) {
-		console.log('Fetch error', error);
+		console.log('Fetch error :(', error);
 	};
 };
 
