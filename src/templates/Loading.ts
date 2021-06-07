547a77f0af157d8f38bcfca3a471bde09a07047c
Aldo Camacho <aldo.acamacho98@gmail.com>
2021-06-07 10:53:38 -0700

    Loading message styles

--- src/templates/Loading.ts
+++ src/templates/Loading.ts
@@ -1,8 +1,11 @@
 const Loading = () => {
-	const element = document.createElement("h3");
-	element.innerText = "Loading";
-
+	const element = document.createElement("div");
 	element.id = "loading";
+
+	const inner = document.createElement("h3");
+	inner.innerText = "Loading";
+
+	element.appendChild(inner);
 
 	return element;
 };
